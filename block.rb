db87eb6f9b304e319e62c66db3098f744c396826
Jayden Kang <beprajna@70-12-246-7.pools.spcsdns.net>
2018-04-27 21:05:50 +0900

    0000 hashed minig

--- block.rb
+++ block.rb
@@ -3,8 +3,11 @@ class Blockchain
 
 	def mining
 		begin
-			nonce = rand(10)
-		end while nonce !=0
-		
+			nonce = rand(100000)
+			hashed = Digest::SHA256.hexdigest(nonce.to_s)
+		end while hashed[0..3] != "0000" #hashed 값의 첫 0-3사이의 값이 0000 일때까지
+
+		nonce
 	end
+
 end
--- server.rb
+++ server.rb
@@ -6,11 +6,14 @@ require './block'
 b = Blockchain.new
 
 
+
 get '/' do
 	"블럭 리스트 입니다."
 end
 
 
 get '/mine' do
-	"마이닝 중입니다."
+	
+ b.mining.to_s
+
 end
