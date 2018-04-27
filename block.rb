aeeafea20f311b0f119e918cf9608fbe7203e57c
Jayden Kang <beprajna@70-12-246-7.pools.spcsdns.net>
2018-04-27 20:40:16 +0900

    랜덤하게 10 중 1개

--- block.rb
+++ block.rb
@@ -1,1 +1,10 @@
-aaaa
+class Blockchain
+
+
+	def mining
+		begin
+			nonce = rand(10)
+		end while nonce !=0
+		
+	end
+end
--- server.rb
+++ server.rb
@@ -1,5 +1,16 @@
 require 'sinatra'
+require './block' 
+#block.rb 연결
+
+
+b = Blockchain.new
+
 
 get '/' do
 	"블럭 리스트 입니다."
+end
+
+
+get '/mine' do
+	"마이닝 중입니다."
 end
